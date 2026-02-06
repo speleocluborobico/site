54d407cad4a0c2e6d3254368e9920216fdf1eaba
Marco Frassinelli <marco.frassinelli@accenture.com>
2026-02-06 12:54:27 +0100

    fix: base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import starlightImageZoom from 'starlight-image-zoom';
 
 // https://astro.build/config
 export default defineConfig({
+	base: "/site",
 	integrations: [
 		starlight({
 			plugins:  [
